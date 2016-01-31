6543e5bc7a48a5306c9679853689e75b624438ea
shellrock <hanwoody@gmail.com>
2016-01-31 17:04:36 +0800

    fix haproxy path

--- reload.sh
+++ reload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pidfile=/var/run/haproxy.pid
 cfgfile=$1
-exec /usr/local/sbin/haproxy -D -f $cfgfile -p $pidfile -sf $(cat $pidfile)
+exec /usr/sbin/haproxy -D -f $cfgfile -p $pidfile -sf $(cat $pidfile)
