ce241f66d3ef0122c077e8862b0953224b4ac65e
shellrock <hanwoody@gmail.com>
2016-02-01 11:38:59 +0800

    when gracefully restart haproxy, be careful of old pids

--- reload.sh
+++ reload.sh
@@ -2,6 +2,12 @@
 
 PORTS=80:443
 iptables -I INPUT -p tcp -m multiport --dports $PORTS --syn -j DROP
+
 sleep 1
-/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -sf $(cat /var/run/haproxy.pid)
+old_pid=$(cat /var/run/haproxy.pid)
+if [ -z "$old_pid" ]; then
+  old_pid=$(ps ax|grep '/usr/sbin/haprox[y]'|cut -c1-5)
+fi
+/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -sf $old_pid
+
 iptables -D INPUT -p tcp -m multiport --dport $PORTS --syn -j DROP
