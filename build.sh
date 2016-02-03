0639e12a29aba1ba3f4af4303b6bb7becfbf112c
shellrock <hanwoody@gmail.com>
2016-02-03 14:41:53 +0800

    add test

--- build.sh
+++ build.sh
@@ -3,19 +3,23 @@
 set -e
 
 export CON_NAME=haproxy_t
-export REG_URL=d.nicescale.com:5000
-export IMAGE=haproxy
-export TAGS="1.6"
+export REG_URL=index.csphere.cn
+export IMAGE=microimages/haproxy
+export TAGS="1.6 1.6.2"
 export BASE_IMAGE=microimages/alpine
 
 docker pull $BASE_IMAGE
 
-docker build -t microimages/$IMAGE .
+docker build -t $IMAGE .
+./test.sh
 
-#./test.sh
+docker tag -f $IMAGE $REG_URL/$IMAGE
 
 for t in $TAGS; do
-  docker tag -f microimages/$IMAGE microimages/$IMAGE:$t
+  docker tag -f $IMAGE $REG_URL/$IMAGE:$t
+  docker tag -f $IMAGE $IMAGE:$t
 done
 
-docker push microimages/$IMAGE
+docker push $IMAGE
+docker push $REG_URL/$IMAGE
+
---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+docker rm -f "$CON_NAME" > /dev/null 2>&1
+docker run -d --name $CON_NAME $IMAGE
+
+sleep 1
+
+docker exec $CON_NAME ps ax|grep "haprox[y]"
+docker exec $CON_NAME ps ax|grep "conf[d]"
+docker exec $CON_NAME wget -O - http://localhost/
+
+docker rm -f $CON_NAME
+
+echo "---> The test pass"
