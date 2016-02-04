a7fdb9635d7d26fcf4d0321fb89ec3b5295ccb28
shellrock <hanwoody@gmail.com>
2016-02-04 23:13:47 +0800

    fix tet

--- test.sh
+++ test.sh
@@ -3,13 +3,11 @@
 set -xe
 
 docker rm -f "$CON_NAME" > /dev/null 2>&1 || true
-docker run -d --name $CON_NAME $IMAGE
+docker run -d -e APP=web -e SERVICE=service -e PUBLIC_DOMAIN=a.com -e HOST_IP=172.17.0.1 --name $CON_NAME $IMAGE
 
 sleep 12
 
-docker exec $CON_NAME ps ax|grep "haprox[y]"
 docker exec $CON_NAME ps ax|grep "conf[d]"
-docker exec $CON_NAME wget -O - http://localhost/
 
 docker rm -f $CON_NAME
 
