b834de74507b01ce52e719fa688b39877c566663
Administrator <admin@example.com>
2018-08-29 16:45:30 +0800

    fix

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ set -e
 
 export CON_NAME=haproxy_t
 export REG_URL=index.csphere.cn
+export IREG_URL=docker.nicescale.com:4443
 export IMAGE=microimages/haproxy
-export TAGS="1.6-https 1.6.6-https"
+export TAGS="1.8"
 export BASE_IMAGE=microimages/alpine
 
 docker pull $BASE_IMAGE
@@ -17,9 +18,10 @@ docker tag -f $IMAGE $REG_URL/$IMAGE
 
 for t in $TAGS; do
   docker tag -f $IMAGE $REG_URL/$IMAGE:$t
+  docker tag -f $IMAGE $IREG_URL/$IMAGE:$t
   docker tag -f $IMAGE $IMAGE:$t
 done
 
-docker push $IMAGE
 docker push $REG_URL/$IMAGE
+docker push $IREG_URL/$IMAGE
 
